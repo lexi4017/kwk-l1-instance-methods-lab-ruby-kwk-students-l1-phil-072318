f1c795b9e6d16c142cd6c45aba8b8771d324bd49
Alexsia Light <elightl729@comcast.net>
2018-07-26 18:46:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # dog.rb
+
+  class Dog
+
+def initialize(name)
+  @name = name
+  end
+def bark
+  puts "Woof!"
+  
+end 
+
+fido = Dog.new("Fido")
+snoopy = Dogs.new("Snoopy")
+lassie = Dog.new("Lassie")
+
+bark
+  
